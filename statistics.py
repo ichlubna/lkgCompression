4c9be14093b40bc4623afde2c962ad0a94c8e9ad
Tomáš Chlubna <ichlubna@fit.vutbr.cz>
2025-02-08 16:05:25 +0100

    Add files via upload

--- statistics.py
+++ statistics.py
@@ -1,9 +1,13 @@
 import numpy as np
 import dcor
+from sklearn.feature_selection import mutual_info_regression
+
 nums = "0.01153846154	-0.01153846154	0.05	0.3038461538	0.6923076923	0.8576923077"
-nums2 = "0.00813971061	0.02276380594	0.03046782787	0.04582006565	0.0413766573	0.02121449232"
+nums2 = "0.001802270032	0.009638958333	0.02735195353	0.04948666026	0.08705970673	0.1069567091"
 a = np.array(nums.split(), dtype=float)
 b = np.array(nums2.split(), dtype=float)
 print(a)
 print(b)
-print(dcor.distance_correlation(a, b))
+#print(dcor.distance_correlation(a, b))
+mi = mutual_info_regression(a.reshape(-1, 1), b)
+print(mi[0])
